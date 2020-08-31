e9c571ebb594afb67c526ef39bd76145c2ce6bc7
Agustin Aguilar <Agusxrun@gmail.com>
2020-08-31 14:29:42 +0200

    Disable web3 providers

--- src/constants/index.js
+++ src/constants/index.js
@@ -86,50 +86,50 @@ const TESTNET_CAPABLE_WALLETS = {
 export const SUPPORTED_WALLETS = {
   ...TESTNET_CAPABLE_WALLETS,
   ...{
-    WALLET_CONNECT: {
-      connector: walletconnect,
-      name: 'WalletConnect',
-      iconName: 'walletConnectIcon.svg',
-      description: 'Connect to Trust Wallet, Rainbow Wallet and more...',
-      href: null,
-      color: '#4196FC',
-      mobile: true
-    },
-    WALLET_LINK: {
-      connector: walletlink,
-      name: 'Coinbase Wallet',
-      iconName: 'coinbaseWalletIcon.svg',
-      description: 'Use Coinbase Wallet app on mobile device',
-      href: null,
-      color: '#315CF5'
-    },
-    COINBASE_LINK: {
-      name: 'Open in Coinbase Wallet',
-      iconName: 'coinbaseWalletIcon.svg',
-      description: 'Open in Coinbase Wallet app.',
-      href: 'https://go.cb-w.com/mtUDhEZPy1',
-      color: '#315CF5',
-      mobile: true,
-      mobileOnly: true
-    },
-    FORTMATIC: {
-      connector: fortmatic,
-      name: 'Fortmatic',
-      iconName: 'fortmaticIcon.png',
-      description: 'Login using Fortmatic hosted wallet',
-      href: null,
-      color: '#6748FF',
-      mobile: true
-    },
-    Portis: {
-      connector: portis,
-      name: 'Portis',
-      iconName: 'portisIcon.png',
-      description: 'Login using Portis hosted wallet',
-      href: null,
-      color: '#4A6C9B',
-      mobile: true
-    }
+    // WALLET_CONNECT: {
+    //   connector: walletconnect,
+    //   name: 'WalletConnect',
+    //   iconName: 'walletConnectIcon.svg',
+    //   description: 'Connect to Trust Wallet, Rainbow Wallet and more...',
+    //   href: null,
+    //   color: '#4196FC',
+    //   mobile: true
+    // },
+    // WALLET_LINK: {
+    //   connector: walletlink,
+    //   name: 'Coinbase Wallet',
+    //   iconName: 'coinbaseWalletIcon.svg',
+    //   description: 'Use Coinbase Wallet app on mobile device',
+    //   href: null,
+    //   color: '#315CF5'
+    // },
+    // COINBASE_LINK: {
+    //   name: 'Open in Coinbase Wallet',
+    //   iconName: 'coinbaseWalletIcon.svg',
+    //   description: 'Open in Coinbase Wallet app.',
+    //   href: 'https://go.cb-w.com/mtUDhEZPy1',
+    //   color: '#315CF5',
+    //   mobile: true,
+    //   mobileOnly: true
+    // },
+    // FORTMATIC: {
+    //   connector: fortmatic,
+    //   name: 'Fortmatic',
+    //   iconName: 'fortmaticIcon.png',
+    //   description: 'Login using Fortmatic hosted wallet',
+    //   href: null,
+    //   color: '#6748FF',
+    //   mobile: true
+    // },
+    // Portis: {
+    //   connector: portis,
+    //   name: 'Portis',
+    //   iconName: 'portisIcon.png',
+    //   description: 'Login using Portis hosted wallet',
+    //   href: null,
+    //   color: '#4A6C9B',
+    //   mobile: true
+    // }
   }
 }
 
